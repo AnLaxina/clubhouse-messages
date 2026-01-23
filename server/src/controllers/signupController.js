3069ed8caba0b10f97f7323b63b6b042c66cba0b
AnLaxina <anilovlaxina@gmail.com>
2026-01-23 13:19:48 -0800

    feat(signup): add working db integration

--- server/src/controllers/signupController.js
+++ server/src/controllers/signupController.js
@@ -1,4 +1,14 @@
+import * as db from "../db/queries.js";
+
 export function postValues(req, res, next) {
-  console.log(req.body);
-  res.send({ Success: "Chicken" });
+  const formValues = req.body;
+  db.addUser(
+    formValues.firstName,
+    formValues.lastName,
+    formValues.username,
+    formValues.password,
+    formValues.memberCode,
+    formValues.adminCode,
+  );
+  res.send({ message: "Success!" });
 }
--- server/src/db/queries.js
+++ server/src/db/queries.js
@@ -4,3 +4,28 @@ export async function getAllUsers() {
   const { rows } = await pool.query("SELECT * FROM users");
   return rows;
 }
+
+export async function addUser(
+  firstName,
+  lastName,
+  username,
+  password,
+  memberCode,
+  adminCode,
+) {
+  let memberStatus = false;
+  let adminStatus = false;
+
+  if (memberCode.toLowerCase() === "pikachu") {
+    memberStatus = true;
+  } else if (adminCode.toLowerCase() === "chicken") {
+    memberStatus = true;
+    adminStatus = true;
+  }
+
+  await pool.query(
+    `INSERT INTO users (first_name, last_name, username, password, membership_status, is_admin)
+                    VALUES ($1, $2, $3, $4, $5, $6);`,
+    [firstName, lastName, username, password, memberStatus, adminStatus],
+  );
+}

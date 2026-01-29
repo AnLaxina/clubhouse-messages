86543c79052d73d590a6da04c937747c53e95a37
AnLaxina <anilovlaxina@gmail.com>
2026-01-29 01:39:34 -0800

    feat(messages): add time `x ago` feature for each message

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -1,12 +1,22 @@
 import styles from "./messages.module.css";
 
+import { formatDistanceToNowStrict } from "date-fns";
+
 export default function Messages({ data, isAdmin, isMember }) {
+  function getTimeAgo(messageCreated = new Date()) {
+    const time = formatDistanceToNowStrict(messageCreated, { addSuffix: true });
+    if (time === "0 seconds ago") {
+      return "Now";
+    }
+    return time;
+  }
+
   return (
     <div className={styles.messageContainer}>
       <article className={styles.message}>
         <div className={styles.authorDetails}>
           <h4>Chicken Man</h4>
-          <time dateTime="YYYY-MM-DDTHH:mm:ss.sssZ">3 Days Ago</time>
+          <time dateTime="YYYY-MM-DDTHH:mm:ss.sssZ">{getTimeAgo()}</time>
         </div>
         <h3>Test Message</h3>
         <p>
